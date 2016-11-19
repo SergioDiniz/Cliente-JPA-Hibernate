ac006ace0de21f84a58be8d919a50d0cb1503a0f
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-18 21:17:47 -0300

    add libs and created cliente entity

---
+++ src/beans/Cliente.java
@@ -0,0 +1,67 @@
+package beans;
+
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Cliente {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@Temporal(TemporalType.DATE)
+	private Calendar dataNascimento;
+	
+	public Cliente() {}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the dataNascimento
+	 */
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	/**
+	 * @param dataNascimento the dataNascimento to set
+	 */
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	
+	
+}
