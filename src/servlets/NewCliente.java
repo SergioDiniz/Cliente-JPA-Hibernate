90f3a75c72b2ffdccb21f877c7ffd746e1faa406
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-18 21:38:37 -0300

    created new cliente servlet

---
+++ src/servlets/NewCliente.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Cliente;
+import dao.ClienteDao;
+
+/**
+ * Servlet implementation class NewCliente
+ */
+@WebServlet("/newCliente")
+public class NewCliente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewCliente() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Cliente cliente = new Cliente();
+		cliente.setDataNascimento(Calendar.getInstance());
+		cliente.setNome(request.getParameter("nome"));
+		
+		ClienteDao dao = new ClienteDao();
+		dao.addCliente(cliente);
+		
+		response.sendRedirect("listar-clientes.jap");
+		
+	}
+
+}
